a692b1768d3ae9744166d307690b4595a5afae7a
Bo Lorentsen <bo.lorentsen@watts.dk>
2025-11-12 13:43:12 +0100

    make watts ui control work again

--- src/main.c
+++ src/main.c
@@ -1,31 +1,22 @@
+
 /**
- * @file main.c
+ * @file main
  *
  */
 
 /*********************
  *      INCLUDES
  *********************/
-
-#ifndef _DEFAULT_SOURCE
-  #define _DEFAULT_SOURCE /* needed for usleep() */
-#endif
-
+#define _DEFAULT_SOURCE /* needed for usleep() */
 #include <stdlib.h>
 #include <stdio.h>
-#ifdef _MSC_VER
-  #include <Windows.h>
-#else
-  #include <unistd.h>
-  #include <pthread.h>
-#endif
+#include <unistd.h>
+#include <pthread.h>
 #include "lvgl/lvgl.h"
-//#include "lvgl/examples/lv_examples.h"
-#include "ui_watts_charger.h"
+#include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
-#include <SDL.h>
-
-#include "hal/hal.h"
+#include "ui_watts_charger.h"
+#include "control_panel.h"
 
 /*********************
  *      DEFINES
@@ -38,7 +29,7 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static lv_display_t * default_display_init(int32_t w, int32_t h);
+static lv_display_t * hal_init(int32_t w, int32_t h);
 
 /**********************
  *  STATIC VARIABLES
@@ -52,7 +43,27 @@ static lv_display_t * default_display_init(int32_t w, int32_t h);
  *   GLOBAL FUNCTIONS
  **********************/
 
-#if LV_USE_OS != LV_OS_FREERTOS
+extern void freertos_main(void);
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *      VARIABLES
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
 
 int main(int argc, char **argv)
 {
@@ -63,38 +74,37 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  default_display_init(480, 480);
+  lv_display_t * disp_main = hal_init(480, 480);
 
   #if LV_USE_OS == LV_OS_NONE
-  sdl_hal_init(320, 480);
 
-  /* Run the default demo */
-  /* To try a different demo or example, replace this with one of: */
-  /* - lv_demo_benchmark(); */
-  /* - lv_demo_stress(); */
-  /* - lv_example_label_1(); */
-  /* - etc. */
   // lv_demo_widgets();
+
+  /*Uncomment the following lines to enable the control panel display for testing UI*/
+  lv_display_t * disp_ctrl = hal_init(900, 750);
+  ui_subjects_init();
+  lv_display_set_default(disp_ctrl);
+  create_control_panel(disp_ctrl);
+
+  lv_display_set_default(disp_main);
   ui_watts_charger_init();
+
   while(1) {
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
-    uint32_t sleep_time_ms = lv_timer_handler();
-    if(sleep_time_ms == LV_NO_TIMER_READY){
-	sleep_time_ms =  LV_DEF_REFR_PERIOD;
-    }
-#ifdef _MSC_VER
-    Sleep(sleep_time_ms);
-#else
-    usleep(sleep_time_ms * 1000);
-#endif
+    lv_timer_handler();
+    usleep(5 * 1000);
   }
+
+  #elif LV_USE_OS == LV_OS_FREERTOS
+
+  /* Run FreeRTOS and create lvgl task */
+  freertos_main();
+
+  #endif
 
   return 0;
 }
-
-
-#endif
 
 /**********************
  *   STATIC FUNCTIONS
@@ -104,27 +114,23 @@ int main(int argc, char **argv)
  * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
  * library
  */
-static lv_display_t * default_display_init(int32_t w, int32_t h)
+static lv_display_t * hal_init(int32_t w, int32_t h)
 {
+
   lv_group_set_default(lv_group_create());
 
   lv_display_t * disp = lv_sdl_window_create(w, h);
-
-  lv_sdl_window_set_title(disp, "Charger simulator");
-
-  // This also makes it floating in wayland
-  lv_sdl_window_set_resizeable(disp, false);
 
   lv_indev_t * mouse = lv_sdl_mouse_create();
   lv_indev_set_group(mouse, lv_group_get_default());
   lv_indev_set_display(mouse, disp);
   lv_display_set_default(disp);
 
-//  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
-//  lv_obj_t * cursor_obj;
-//  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
-//  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-//  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
+  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
+  lv_obj_t * cursor_obj;
+  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
+  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
+  // lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
 
   lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
   lv_indev_set_display(mousewheel, disp);
